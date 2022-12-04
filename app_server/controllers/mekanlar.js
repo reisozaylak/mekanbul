86285fa1e7fd4f1e4beef553c6346a286426e944
reisozaylak <redezaylak1@gmail.com>
2022-12-04 17:48:30 +0300

    4 Aralık

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,86 +1,98 @@
+const axios=require('axios');
+var apiSecenekleri ={
+    sunucu: "http://localhost:3000",
+    apiYolu:"/api/mekanlar/",
+};
 var express = require('express');
 var router = express.Router();
 
+var mesafeyiFormatla=function(mesafe){
+    var yeniMesafe,birim;
+    if(mesafe>1){
+        yeniMesafe=parseFloat(mesafe).toFixed(1);
+        birim=" km";
+    }
+    else{
+        yeniMesafe=parseInt(mesafe*1000);
+        birim=" m";
+    }
+    return yeniMesafe+birim;
+};
 
-
-const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', 
-    { 
-    "baslik":"Anasayfa",
-    "sayfaBaslik": {
-    "siteAd":"MekanBul",
-    "slogan":"Civardaki Mekanları Keşfet!"
-    },
-    "mekanlar":[
-        {
-            "ad":"Starbucks",
-            "puan":"3",
-            "adres":"Centrum Garden AVM",
-            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
-            "mesafe":"10km"
+var anaSayfaOlustur=function(res,mekanListesi){
+    var mesaj;
+    if(!(mekanListesi instanceof(Array))){
+        mesaj="!!API HATASI!!";
+        mekanListesi=[];
+    }
+    else{
+        if(!mekanListesi.length){
+            mesaj="Civarda herhangi bir mekan yok";
+        }
+    }
+    res.render("anasayfa",{
+        "baslik":"Anasayfa",
+        "sayfaBaslik":{
+            "siteAd":"Mekanbul",
+            "sloagan":"Mekanları Keşfet",
         },
-        {
-            "ad":"Barida Kafe",
-            "puan":"4",
-            "adres":"SDU Batı Kampüsü",
-            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
-            "mesafe":"1km"
-        },
-
-    ]
-});
+        "mekanlar":mekanListesi,
+        "mesaj":mesaj,
+    });
 };
 
 
-const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', 
-    {
-        "baslik":"Mekan Bilgisi",
-        "mekanBaslik":"Starbucks",
-        "mekanDetay":{
-            "ad":"Starbucks",
-            "puan":"3",
-            "adres":"Centrum Garden AVM",
-            "imkanlar":["Kahve","Çay","Tatlı Çeşitleri"],
-            "koordinatlar":{
-                "enlem":"37.7",
-                "boylam":"30.5"
-            },
-            "saatler":[
-                {
-                "gunler":"Pazartesi-Cuma",
-                "acilis":"9:00",
-                "kapanis":"23:00",
-                "kapali": false
-                },
-                {
-                "gunler":"Cumartesi-Pazar",
-                "acilis":"8:00",
-                "kapanis":"22:00",
-                "kapali": false
-                }
-
-            ],
-          
-            "yorumlar":[
-                {
-                    "yorumYapan":"Reis Özaylak",
-                    "yorumMetni":"KÖTÜ",
-                    "yorumTarihi":"20 Ekim 2022",
-                    "puan":"2"
-                },
-                {
-                    "yorumYapan":"Reis Özaylak",
-                    "yorumMetni":"berbatttt",
-                    "yorumTarihi":"20 Ekim 2022",
-                    "puan":"2"
-                }
-            ]
-           
-
-
+const anaSayfa=function(req, res, next) {
+    axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+        params:{
+            enlem:req.query.enlem,
+            boylam:req.query.boylam,
         }
-     });
+    }).then(function(response){
+        var i,mekanlar;
+        mekanlar=response.data;
+        for(i=0;i<mekanlar.length;i++){
+            mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+        }
+        anaSayfaOlustur(res,mekanlar);
+    }).catch(function(hata){
+        anaSayfaOlustur(res,hata);
+    });
+};
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+    mekanDetaylari.koordinat={
+        "enlem":mekanDetaylari.koordinat[0],
+        "boylam":mekanDetaylari.koordinat[1],
+    }
+    res.render('mekanbilgisi',
+    {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
+    });
+}
+var hataGoster = function(res,hata){
+    var mesaj;
+    if(hata.response.status==404){
+        mesaj="404, Sayfa Bulunamadı!";
+    }
+    else{
+        mesaj=hata.response.status+" hatası";
+    }
+    res.status(hata.response.status);
+    res.render('error',{
+        "mesaj":mesaj
+    });
+};
+
+const mekanBilgisi=function(req, res) {
+    axios
+    .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+    .then(function(response){
+        detaySayfasiOlustur(res,response.data);
+    })
+    .catch(function(hata){
+        hataGoster(res,hata);
+    });
 };
 
 const yorumEkle=function(req, res, next) {
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,8 @@ var ctrlMekanlar=require('../controllers/mekanlar');
 var ctrlDigerleri=require('../controllers/digerleri');
 
 
-
-
-/* GET home page. */
 router.get('/',ctrlMekanlar.anaSayfa);
-router.get('/mekan',ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/:mekanid',ctrlMekanlar.mekanBilgisi);
 router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
 router.get('/hakkinda',ctrlDigerleri.hakkinda);
 
